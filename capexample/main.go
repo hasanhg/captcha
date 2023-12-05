5a375a6a42949d70147c5affabed11a4d6b7b4f4
hasan gürsoy <hasan@robomotion.io>
2023-12-05 17:09:29 +0300

    module fi

--- capexample/main.go
+++ capexample/main.go
@@ -7,7 +7,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/dchest/captcha"
+	"github.com/hasanhg/captcha"
 	"io"
 	"log"
 	"net/http"
--- capgen/main.go
+++ capgen/main.go
@@ -8,7 +8,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/dchest/captcha"
+	"github.com/hasanhg/captcha"
 	"io"
 	"log"
 	"os"
